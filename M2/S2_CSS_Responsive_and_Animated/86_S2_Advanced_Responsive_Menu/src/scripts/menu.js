9701f73f0a5ab813d689eae1c52d34474c439e4d
bernardoenock <bernardoenockgm@gmail.com>
2023-11-22 18:28:39 -0300

    fix: Clean

--- M2/S2_CSS_Responsive_and_Animated/86_S2_Advanced_Responsive_Menu/src/scripts/menu.js
+++ M2/S2_CSS_Responsive_and_Animated/86_S2_Advanced_Responsive_Menu/src/scripts/menu.js
@@ -1,6 +1,5 @@
 const burguerMenu = document.querySelector('.hamburger');
 const menu = document.querySelector('.info-menu');
-const closeMenu = document.querySelector('.close')
 
 burguerMenu.addEventListener('click', function() {
   menu.classList.toggle('show');
